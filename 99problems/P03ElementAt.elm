49de37d6cb86d24b7a0454deb959e2ab4993236d
Viet Hung Nguyen <hvn@familug.org>
2018-05-05 18:32:54 +0700

    Add P03 P04

---
+++ 99problems/P03ElementAt.elm
@@ -0,0 +1,16 @@
+module P03ElementAt exposing (..)
+
+{--
+(*) Find the K'th element of a list. The first element in the list is number 1.
+-}
+
+output : String
+output =
+    "Hello World!" |> String.toList |> elementAt 3 |> toString
+
+elementAt : Int -> List a -> Maybe a
+elementAt k list =
+    if k < 0 then
+        Nothing
+    else
+        List.head <| List.drop (k - 1) list
